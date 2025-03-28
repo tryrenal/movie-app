5ec3d843079c82f65d45ec3877ab20fdb966b771
redveloper <renalraion@gmail.com>
2025-03-28 22:19:51 +0700

    [UPDATE] update for show list of movie

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
-import {Image, ScrollView, Text, View} from "react-native";
-import {Link} from "expo-router";
-import {images} from "@/constants/images";
+import {ActivityIndicator, FlatList, Image, ScrollView, Text, View} from "react-native";
+import {useRouter} from "expo-router";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
-import { useRouter } from 'expo-router';
+import {fetchMovies} from "@/services/api";
+import useFetch from "@/services/useFetch";
+import MovieCard from "@/components/MovieCard";
 
 export default function Index() {
 
     const router = useRouter();
+    const { data: movies, loading: moviesLoading, error: moviesError} = useFetch(() => fetchMovies({ query: ''}));
 
     return (
         <View className="flex-1 bg-primary">
@@ -18,13 +20,43 @@ export default function Index() {
                             paddingBottom: 10
                         }}>
                 <Image source={icons.ic_react} className="w-212 h-15 mt-20 mb-5 mx-auto"/>
+                {
+                    moviesLoading? (
+                        <ActivityIndicator size='large' color='#000ff' className='mt-10 self-center'/>
+                    ) : moviesError ? (
+                        <Text>Error: {moviesError?.message}</Text>
+                    ) : (
+                        <View className="flex-1 mt-5">
+                            <SearchBar
+                                onPress={() => router.push("/search")}
+                                placeholder="Search for a movie"
+                            />
 
-                <View className="flex-1 mt-5">
-                    <SearchBar
-                        onPress={() => router.push("/search")}
-                        placeholder="Search for a movie"
-                    />
-                </View>
+                            <>
+                                <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+
+                                <FlatList
+                                    data={movies}
+                                    renderItem={({item}) => (
+                                        <MovieCard
+                                            {...item}
+                                        />
+                                    )}
+                                    keyExtractor={(item) => item.id.toString()}
+                                    numColumns={3}
+                                    columnWrapperStyle={{
+                                        justifyContent: 'flex-start',
+                                        gap:20,
+                                        paddingRight: 5,
+                                        marginBottom: 10
+                                    }}
+                                    className="mt-5 pb-32"
+                                    scrollEnabled={false}
+                                />
+                            </>
+                        </View>
+                    )
+                }
             </ScrollView>
         </View>
     );
---
+++ components/MovieCard.tsx
@@ -0,0 +1,35 @@
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {Link} from "expo-router";
+import {icons} from "@/constants/icons";
+
+const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) => {
+    return (
+        <Link href={`/movies/${id}`} asChild>
+            <TouchableOpacity className="w-[30%]">
+                <Image
+                    source={{
+                        uri: poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                            : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
+                    }}
+                    className="w-full h-52 rounded-lg"
+                    resizeMode="cover"
+                />
+                <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>{title}</Text>
+
+                <View className="flex-row items-center justify-start gap-x-1">
+                    <Image source={icons.ic_star} className="size-4" tintColor="yellow"/>
+                    <Text className="text-white text-xs font-bold uppercase">{Math.round(vote_average / 2)}</Text>
+                </View>
+
+                <View className="flex-row items-center justify-between">
+                    <Text className="text-xs font-medium mt-1 text-light-100">
+                        {release_date?.split('-')[0]}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        </Link>
+    )
+}
+
+export default MovieCard;
